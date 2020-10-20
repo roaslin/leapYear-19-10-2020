1bb7c7984f99e06dec0900bcecc18cbfd8b8f877
roaslin <roaslin@gmail.com>
2020-10-20 10:36:55 +0200

    divisible by 4 and not by 100

--- leapyear.js
+++ leapyear.js
@@ -1,8 +1,13 @@
 function leapYear(number) {
+
+    if(number % 4 === 0 && number % 100 !== 0){
+        return true;
+    }
+
     if(number % 400 === 0){
         return true;
     }
-    
+
     return false;
 }
 
--- leapyear.test.js
+++ leapyear.test.js
@@ -1,5 +1,5 @@
 const isLeapYear = require('./leapyear.js');
-// All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+
 // All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
 // All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
 // All years not divisible by 4 are NOT leap years (e.g. 2017, 2018, 2019).
@@ -11,8 +11,8 @@ describe('leapYear should', ()=>{
         expect(isLeapYear(2400)).toBe(true);
     });
     
-    // test('be divisible by 400', ()=>{
-    //     expect(isLeapYear(2000)).toBe(true);
-    // });
+    test('be divisble by 4 but not by 100', ()=>{
+        expect(isLeapYear(2008)).toBe(true);
+    });
 });
 
